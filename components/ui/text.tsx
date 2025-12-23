aacb2256c049d93b7ef37f186247fac690093212
arun <ptech784@gmail.com>
2025-12-24 01:19:01 +0530

    fix:layout

--- components/ui/text.tsx
+++ components/ui/text.tsx
@@ -1,8 +1,8 @@
-"use client"
+"use client";
 
-import * as React from "react"
-import { cva, type VariantProps } from "class-variance-authority"
-import { cn } from "@/lib/utils"
+import { cn } from "@/lib/utils";
+import { cva, type VariantProps } from "class-variance-authority";
+import * as React from "react";
 
 const textVariants = cva("", {
   variants: {
@@ -26,27 +26,26 @@ const textVariants = cva("", {
     variant: "base",
     weight: "normal",
   },
-})
+});
 
 export interface TextProps
   extends React.HTMLAttributes<HTMLElement>,
     VariantProps<typeof textVariants> {
-  as?: "p" | "span" | "div" | "h1" | "h2" | "h3" | "h4" | "h5" | "h6"
+  as?: "p" | "span" | "div" | "h1" | "h2" | "h3" | "h4" | "h5" | "h6";
 }
 
 const Text = React.forwardRef<HTMLElement, TextProps>(
   ({ className, variant, weight, as = "p", ...props }, ref) => {
-    const Component = as
+    const Component = as;
     return (
       <Component
-        ref={ref as any}
-        className={cn(textVariants({ variant, weight }), className)}
+        ref={ref as React.RefObject<HTMLParagraphElement>}
+        className={cn(textVariants({ variant, weight }), className) as string}
         {...props}
       />
-    )
+    );
   }
-)
-Text.displayName = "Text"
+);
+Text.displayName = "Text";
 
-export { Text, textVariants }
-
+export { Text, textVariants };
--- hooks/useFieldRemove.ts
+++ hooks/useFieldRemove.ts
@@ -1,30 +1,30 @@
-import { removeId } from '@/lib/utils/dataTransformers'
+    import { removeId } from '@/lib/utils/dataTransformers'
 import { useCallback } from 'react'
 import { useResume } from './useResume'
 
-interface UseFieldRemoveProps {
-  remove: (index: number) => void
-  sectionKey: string
-  data: any[]
-}
+    interface UseFieldRemoveProps {
+    remove: (index: number) => void
+    sectionKey: string
+    data: unknown[]
+    }
 
-export const useFieldRemove = ({ remove, sectionKey, data }: UseFieldRemoveProps) => {
-  const { updateResume } = useResume()
-  const handleRemove = useCallback(
-    (index: number) => {
-      const currentItem = data?.[index]
-      if (!currentItem) {
+    export const useFieldRemove = ({ remove, sectionKey, data }: UseFieldRemoveProps) => {
+    const { updateResume } = useResume()
+    const handleRemove = useCallback(
+        (index: number) => {
+        const currentItem = data?.[index]
+        if (!currentItem) {
+            remove(index)
+            return
+        }
+
+        const updatedArray = data
+            .filter((_, index_) => index_ !== index)
+            .map((item: unknown) => removeId(item as { id?: string }))
         remove(index)
-        return
-      }
-
-      const updatedArray = data
-        .filter((_, index_) => index_ !== index)
-        .map((item) => removeId(item))
-      remove(index)
-      updateResume({ [sectionKey]: updatedArray })
-    },
-    [remove, sectionKey, data, updateResume]
-  )
-  return handleRemove
-}
+        updateResume({ [sectionKey]: updatedArray })
+        },
+        [remove, sectionKey, data, updateResume]
+    )
+    return handleRemove
+    }
